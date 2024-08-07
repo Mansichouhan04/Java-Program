ec8079c97bc896e15b98b61f1e39d24abea63007
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-07 23:47:29 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,11 @@
+ class ArraySum {
+    public static void main(String[]args){
+         int sum=0;
+        int arr[]={12,15,6,21,8};
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println("sum of all element="+sum);
+    }
+    
+}
