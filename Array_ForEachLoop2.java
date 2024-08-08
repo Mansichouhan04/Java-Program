00738d61f3e421c1fa01a0cf94f851392ec326ef
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-08 23:33:35 +0530

    Add files via upload

---
+++ Array_ForEachLoop2.java
@@ -0,0 +1,13 @@
+package _concept;
+
+public class Array_ForEachLoop2 {
+    public static void main(String[]args){
+        String arr[]={"mansi","varsha","saloni","shivani","diksha"};
+        for(String s:arr){
+            System.out.println(s);
+        }
+
+    }
+}
+    
+
