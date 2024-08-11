7d218ff1de82935faa18274fb15b8f9760c4d117
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-11 21:50:35 +0530

    Add files via upload

---
+++ String_Comparison_equalsMethod.java
@@ -0,0 +1,16 @@
+package _concept;
+
+public class String_Comparison_equalsMethod {
+    public static void main(String[]args){
+        String s1="Mansi";
+        String s2="Mansi";
+        String s3="mansi";
+        String s4="Ayati";
+        String s5="Ayati";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+        System.out.println(s4.equals(s5));
+    }
+    
+}
