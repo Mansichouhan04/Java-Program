0d38558cde444bafe7ff3bb11db00c5c3f0fc3a8
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-11 21:52:03 +0530

    Add files via upload

---
+++ String_comparisonIgnoreCase.java
@@ -0,0 +1,10 @@
+package _concept;
+
+public class String_comparisonIgnoreCase {
+    public static void main(String[]args){
+        String s1="Mansi";
+        String s2="MANSI";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+}
