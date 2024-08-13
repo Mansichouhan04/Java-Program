f39ad4dfd04d255e407807bb52fbd56c4149a45f
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-13 22:25:19 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if (original == sum) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+}
+    
+
