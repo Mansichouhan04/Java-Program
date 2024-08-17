c810918caba51bdb949ed16bf2f40dfce49866c1
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-17 23:31:55 +0530

    Add files via upload

---
+++ Character_Count.java
@@ -0,0 +1,15 @@
+//package _concept.Array_StringPractice;
+
+public class Character_Count {
+    public static void main(String[] args) {
+        String s="Hello123";
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>='A'&& s.charAt(i)<='Z'||s.charAt(i)>='a'&& s.charAt(i)<='z'){
+                count++;
+            }
+        }
+        System.out.println("The character count is:"+count);
+    }
+}
