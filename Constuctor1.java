89ebe0352d893e0604308eeb7054eb49af47b832
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-23 22:26:56 +0530

    Add files via upload

---
+++ Constuctor1.java
@@ -0,0 +1,21 @@
+package _concept;
+class Const {
+    Const(){
+        String color;
+        System.out.println("Red");
+    }
+    Const(String name, int age){
+        System.out.println(name+" "+age);
+    }
+    public static void main(String[] args) {
+        Const c1=new Const();
+        Const c2=new Const("Mansi",21);
+    }
+    }
+    
+
+
+
+
+    
+
