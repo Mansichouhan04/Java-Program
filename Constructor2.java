3e144849363ab3caa722bfc126d7e71fe947d275
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-23 22:29:07 +0530

    Add files via upload

---
+++ Constructor2.java
@@ -0,0 +1,12 @@
+package _concept;
+class Default {
+    Default(){
+    System.out.println("Hello Good Morning");
+    }
+     public static void main(String[] args) {
+        Default d=new Default();
+    }
+}
+
+    
+
