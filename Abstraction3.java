633998044e421da170349f4843dbdfb9fbb4fc27
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-24 22:23:19 +0530

    Update Abstraction3.java

--- Abstraction3.java
+++ Abstraction3.java
@@ -1,11 +1,7 @@
 package _concept;
 abstract class School {
     public abstract void go();
-  //  public void study(){
-//System.out.println("Student is study");
-
-//}
-}
+ }
  class Student5 extends School{
     public void go(){
         System.out.println("Student is going to school");
@@ -15,6 +11,5 @@ class Demo{
         public static void main(String[] args) {
         Student5 s=new Student5();
         s.go();
-        //s.study();
+    }
 }
- }
