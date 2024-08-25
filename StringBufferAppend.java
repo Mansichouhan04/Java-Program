9933266cc16d84d69205f5f87401f693ae963856
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-08-25 22:51:00 +0530

    Add files via upload

---
+++ StringBufferAppend.java
@@ -0,0 +1,9 @@
+package _concept;
+
+public class StringBufferAppend {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.append(" Java");//now original string is changed....
+        System.out.println(sb);//prints hello java.....
+    }
+}
