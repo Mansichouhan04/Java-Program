9a5f798c2ec86745cba601e891ebbd79121d4db8
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-09-09 23:20:19 +0530

    Add files via upload

---
+++ _13Unchecked.java
@@ -0,0 +1,10 @@
+package _concept.Assignment_ExceptionHandling;
+class Unchecked_Example {
+    public static void main(String args[])
+    {
+        int x = 0;
+        int y = 10;
+        int z = y / x;
+        System.out.println(z);
+    }
+}
