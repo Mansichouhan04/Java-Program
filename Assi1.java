fb844701feb84e30b3f7558e2d98a926397b1c0d
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-09-21 23:36:53 +0530

    Add files via upload

---
+++ Assi1.java
@@ -0,0 +1,13 @@
+//1question
+import java.util.ArrayList;
+
+public class Assi1 {
+    public static void main(String[] args) {
+        ArrayList<String>ob=new ArrayList<>();
+        ob.add("Orange");
+        ob.add("Red");
+        ob.add("Blue");
+        System.out.println(ob);
+    }
+    
+}
