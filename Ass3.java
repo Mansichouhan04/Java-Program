d65fb2cab33565c03b6e9ddad82e67d48960b79c
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-09-22 23:08:59 +0530

    Add files via upload

--- Ass3.java
+++ Ass3.java
@@ -1,29 +1,13 @@
-package _concept.StringAssignment;
-/*3.Write a Java program to compare two strings lexicographically, ignoring case differences.
-String 1: This is exercise 1                                                                                  
-String 2: This is Exercise 1                                                                                  
-"This is exercise 1" is equal to "This is Exercise 1"*/
+import java.util.ArrayList;
+
 public class Ass3 {
-    public static void main(String[]args)
-    {
-        String s1="This is exercise 1";
-        String s2="This is Exercise 1";
-        int Comparison = s1.compareToIgnoreCase(s2);
-        
-        if(Comparison<0)
-        {
-            System.out.println(s1+" "+"is less than"+" "+s2);
-        }
-        else if(Comparison>0)
-        {
-            System.out.println(s1+" "+"is grater than"+" "+s2);
-        }
-        else{
-            System.out.println(s1+" "+"is equal to the"+" " +s2+" "+"When ignoring case");
-           
-        }
+         public static void main(String[] args) {
+        ArrayList<String>ob=new ArrayList<>();
+        ob.add("Orange");
+        ob.add("Red");
+        ob.add("Blue");
+        ob.addFirst("Pink");
+        System.out.println(ob);
     }
-}
-
     
-
+}
