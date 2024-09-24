e35d52eb6f872351474eb2729d11d724dd168f36
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-09-24 22:37:01 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,11 @@
+package _concept.Patterns_Program;
+
+public class Simple {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            System.out.print("*");
+        }
+    }
+    
+}
