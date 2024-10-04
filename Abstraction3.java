5cd3ed3f871ca28d6d2da0db45019ed13c1849d2
Mansichouhan04 <mansichouhan6789@gmail.com>
2024-10-04 22:35:18 +0530

    Add files via upload

--- Abstraction3.java
+++ Abstraction3.java
@@ -1,7 +1,11 @@
 package _concept;
 abstract class School {
     public abstract void go();
- }
+  //  public void study(){
+//System.out.println("Student is study");
+
+//}
+}
  class Student5 extends School{
     public void go(){
         System.out.println("Student is going to school");
@@ -11,5 +15,6 @@ class Demo{
         public static void main(String[] args) {
         Student5 s=new Student5();
         s.go();
-    }
+        //s.study();
 }
+ }
